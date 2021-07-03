08f3e6673739a4a3efc3165870a483b9caa8deac
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-02 23:59:58 -0300

    Estruturando o Header

--- src/components/partials/header/index.js
+++ src/components/partials/header/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { HeaderArea } from './styles';
 
 const Header = () => {
     return ( 
         <HeaderArea>
-            <h1>Header</h1>
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">M</span>
+                        <span className="logo-2">K</span>
+                        <span className="logo-3">T</span>
+                    </Link>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="">Create an account</Link>
+                        </li>
+                        <li>
+                            <Link to="" className="button">Post an ad</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </HeaderArea>
      );
 }
--- src/components/partials/header/styles.js
+++ src/components/partials/header/styles.js
@@ -1,6 +1,84 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.header`
-    height: 50px;
-    background-color: darkcyan;
+    height: 60px;
+
+    border-bottom: 1px solid #ccc;
+    background-color: #fff;
+
+    .container {
+        max-width: 1000px;
+        margin: auto;
+
+        display: flex;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .logo {
+        height: 60px;
+
+        flex: 1;
+        display: flex;
+        align-items: center;
+
+        .logo-1,
+        .logo-2,
+        .logo-3 {
+            font-size: 27px;
+            font-weight: 400;
+        }
+
+        .logo-1 {
+            color: #ff0000;
+        }
+        .logo-2 {
+            color: #00ff00;
+        }
+        .logo-3 {
+            color: #0000ff;
+        }
+    }
+
+    nav {
+        padding: 10px 0;
+
+        ul {
+            height: 40px;
+            display: flex;
+            align-items: center;
+            
+            li{
+                margin: 0 20px;
+                padding: 0;
+                list-style: none;
+            
+                a {
+                    font-size: 14px;
+                    font-weight: 400;
+                    color: #000;
+
+                    &:hover {
+                        color: #999;
+                    }
+
+                    &.button {
+                        padding: 5px 10px;
+                        border-radius: 4px;
+                        
+                        color: #fff;
+                        background-color: #ff8100;
+                        
+                        &:hover {
+                            background-color: #e57706;
+                        } 
+                    }
+                }
+            }
+        }
+
+
+    }
 `;
