a085f5cf32c3e3d4267f9c84e83dc9699dcec706
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-14 20:48:21 -0300

    Aplicando responsividade ao ads

--- src/pages/ads/styles.js
+++ src/pages/ads/styles.js
@@ -114,6 +114,34 @@ const PageArea = styled.div`
             }
         }
     }
+
+    @media (max-width: 600px) {
+        flex-direction: column;
+
+        .leftSide {
+            width: auto;
+            margin: 10px;
+
+            ul {
+                display: flex;
+                flex-wrap: wrap;
+
+                li {
+                    width: 50%;
+                }
+            }
+        }
+
+        .rightSide {
+            margin: 10px;
+
+            .list {
+                .aditem {
+                    width: 50%;
+                }
+            }
+        }
+    }
 `;
 
 export { PageArea };
